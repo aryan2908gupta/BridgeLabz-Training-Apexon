079e5fcfb7a1ce20a6f1b053bd88bec437b80120
Aryan Gupta <aryangupta2908@gmail.com>
2025-12-24 13:10:04 +0530

    Delegate first code

---
+++ EventDelegates/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace EventDelegates
+{
+    public delegate void Notify(string message);
+    internal class Program
+    {
+        static void sendEmail(string msg)
+        {
+            Console.WriteLine($"Email : {msg}");
+        }
+
+        static void sendSms(string msg) {
+            Console.WriteLine($"SMS : {msg}");
+        }
+        static void Main(string[] args)
+        {
+            Notify notify;
+            notify = sendEmail;
+            notify += sendSms; // Multicast Delegate
+
+            notify("Order Placed");
+            
+        }
+    }
+}
