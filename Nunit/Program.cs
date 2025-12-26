e563ce39168ad5dfbb0f85e3379487cd7edd984b
Aryan Gupta <aryangupta2908@gmail.com>
2025-12-27 00:19:33 +0530

    NUNIT

---
+++ Nunit/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace NunitCal
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+          Calculator cal = new Calculator();
+            Console.WriteLine("Enter Marks : ");
+            int grade = int.Parse(Console.ReadLine());  
+           string res =  cal.GetGradeByPercentage(grade);
+            Console.WriteLine(res);
+        }
+    }
+}
