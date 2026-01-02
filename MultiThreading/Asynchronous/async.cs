0aabff01efaf15e73a4d8e826021a08f4871e0a8
Aryan Gupta <aryangupta2908@gmail.com>
2026-01-03 02:51:08 +0530

    Async

---
+++ MultiThreading/Asynchronous/async.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiThreading.Asynchronous
+{
+    internal class async
+    {
+        static void Run()
+        {
+            Console.WriteLine("Main Method Started...");
+            SomeMethod();
+            Console.WriteLine("Main Method End");
+            Console.ReadKey();
+        }
+
+        public async static void SomeMethod()
+        {
+            Console.WriteLine("Some Method Started...");
+            SomeMethod2();
+            await Task.Delay(10000); // Non-blocking
+            Console.WriteLine("Some Method End");
+        }
+        public async static Task SomeMethod2()
+        {
+            Console.WriteLine("Some Method2 Started...");
+            await Task.Delay(10000); // Non-blocking
+            Console.WriteLine("Some Method2 End");
+        }
+    }
+}
